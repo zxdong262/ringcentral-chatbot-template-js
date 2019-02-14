36fd4ce81175970ab0d692d6d58022ae8d42fa65
ZHAO Xudong <zxdong@gmail.com>
2019-02-14 16:44:18 +0800

    save

--- src/app.js
+++ src/app.js
@@ -1,10 +1,8 @@
 
 const createApp = require('ringcentral-chatbot/dist/apps').default
-const initDb = require('./init-db')
 const handle = require('./handler')
 
 const app = createApp(handle)
-let port = process.env.RINGCENTRAL_CHATBOT_EXPRESS_PORT
 
 app.get('/', (req, res) => {
   res.end('server running')
@@ -13,7 +11,3 @@ app.get('/favicon.ico', (req, res) => {
   res.end('')
 })
 app.get('/test', (req, res) => res.send('server running'))
-app.listen(port, () => {
-  console.log('bot server running on', `http://localhost:${port}`)
-  initDb()
-})
