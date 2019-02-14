c42a15baafd1528f6e4d465350f43cba4734f5db
ZHAO Xudong <zxdong@gmail.com>
2019-02-14 18:13:56 +0900

    Support lambda (#2)

--- src/app.js
+++ src/app.js
@@ -1,10 +1,8 @@
 
-const createApp = require('ringcentral-chatbot/dist/apps').default
-
-const handle = require('./handler')
+import createApp from 'ringcentral-chatbot/dist/apps'
+import handle from './handler'
 
 const app = createApp(handle)
-let port = process.env.RINGCENTRAL_CHATBOT_EXPRESS_PORT
 
 app.get('/', (req, res) => {
   res.end('server running')
@@ -12,6 +10,6 @@ app.get('/', (req, res) => {
 app.get('/favicon.ico', (req, res) => {
   res.end('')
 })
-app.listen(port, () => {
-  console.log('bot server running on', `http://localhost:${port}`)
-})
+app.get('/test', (req, res) => res.send('server running'))
+
+export default app
